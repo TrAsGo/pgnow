2802ee50b547a8af49a2a8020d31c853ebdf01b8
TrAsGo <federico.kamelhar@warbyparker.com>
2019-02-22 10:34:47 -0500

    Initial Settings

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
      description="A Tool to Integrate PG8000 into your application",
      long_description=long_description,
    long_description_content_type="text/markdown",
-     url="https://github.com/javatechy/dokr",
+     url="https://github.com/TrAsGo/pgnow",
      packages=setuptools.find_packages(),
      classifiers=[
          "Programming Language :: Python :: 3",
